746c1dc77647c477fa88828a651e768f97635ace
isaacs <i@izs.me>
2020-09-09 14:59:01 -0700

    Allow unknown envs in configs to be passed through
    If a user does something like `npm exec -c 'echo ${FOO}'` then we should
    not fail if there is no FOO environment variable, and instead just pass
    it through as-is.

--- test/env-replace.js
+++ test/env-replace.js
@@ -8,6 +8,6 @@ const env = {
 
 t.equal(envReplace('\\${foo}', env),  '${foo}')
 t.equal(envReplace('\\\\${foo}', env),  '\\bar')
-t.throws(() => envReplace('${baz}', env), {
-  message: 'Failed to replace env in config: ${baz}',
-})
+t.equal(envReplace('${baz}', env), '${baz}')
+t.equal(envReplace('\\${baz}', env), '${baz}')
+t.equal(envReplace('\\\\${baz}', env), '\\${baz}')
